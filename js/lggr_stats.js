45d0e2c538ca97e579b18d5336aa78a5eedbeaec
Kai Kretschmann <kai@kaikretschmann.de>
2015-06-22 12:15:24 +0000

    charts the jQuery way

--- js/lggr_stats.js
+++ js/lggr_stats.js
@@ -8,13 +8,13 @@ var options = {
 };
 
 
-var ctx = document.getElementById("chartMsgsPerHour").getContext("2d");
+var ctx = $("#chartMsgsPerHour").get(0).getContext("2d");
 var chartMsgsPerHour = new Chart(ctx).Bar(dataMsgsPerHour, options);
 
-ctx = document.getElementById("chartServers").getContext("2d");
+ctx = $("#chartServers").get(0).getContext("2d");
 var chartServers = new Chart(ctx).Bar(dataServers, options);
 
-ctx = document.getElementById("chartLevels").getContext("2d");
+ctx = $("#chartLevels").get(0).getContext("2d");
 var chartLevels = new Chart(ctx).PolarArea(dataLevels, options);
 
 });
