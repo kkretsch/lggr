bf92ec22674e5c3460c682f425f2d6781bea2e2d
Kai Kretschmann <kai@kaikretschmann.de>
2015-07-02 12:25:06 +0000

    pagination till the end

--- tpl/paginate.inc.php
+++ tpl/paginate.inc.php
@@ -2,8 +2,7 @@
   <ul class="pagination">
 <?php
 
-$maxPages = $state->getResultSize() / LggrState::PAGELEN; // maximum pages
-if($maxPages>9) $maxPages=9;
+$maxPages = ceil($state->getResultSize() / LggrState::PAGELEN); // maximum pages
 
 if($page>0) {
         echo '<li><a href="./do.php?a=paginate&page=' . ($page-1) . '" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
@@ -11,13 +10,24 @@ if($page>0) {
         echo '<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a></li>';
 } // if
 
-for($i=0; $i<$maxPages; $i++) {
+if($page-4 > 1) {
+	echo '<li><a href="./do.php?a=paginate&page=0">1</a></li>';
+} // if
+
+for($i=$page-4; $i<$page+4; $i++) {
         if($page == $i) {
-                echo '<li class="active"><a href="./do.php?a=paginate&page=' . $i . '">' . ($i+1) . '</a></li>';
-        } else {
-                echo '<li><a href="./do.php?a=paginate&page=' . $i . '">' . ($i+1) . '</a></li>';
-        } // if
+		$class='active';
+	} else {
+		$class='';
+	} // if
+	if(($i>=0) && ($i<$maxPages)) {
+		echo '<li  class="' . $class . '"><a href="./do.php?a=paginate&page=' . $i . '">' . ($i+1) . '</a></li>';
+	} //if
 } // for i
+
+if($page+4 < $maxPages) {
+	echo '<li><a href="./do.php?a=paginate&page=' . ($maxPages-1) . '">' . $maxPages . '</a></li>';
+} // if
 
 if($page+1 >= $maxPages) {
 	echo '<li class="disabled"><a href="./do.php?a=paginate&page=' . ($page+1) . '" aria-label="Next"><span aria-hidden="true">&raquo;</span></a></li>';
