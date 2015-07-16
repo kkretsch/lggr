9cae74adedbba19887af857d0f6cb088e7b92849
Kai Kretschmann <kai@kaikretschmann.de>
2015-07-16 13:28:01 +0200

    Glyphicons

--- stats.php
+++ stats.php
@@ -50,14 +50,14 @@ require 'tpl/nav.inc.php';
 
 <div class="container">
   <div class="row">
-    <div class="col-md-6"><h2>Messages per hour</h2><canvas id="chartMsgsPerHour"></canvas></div>
-    <div class="col-md-6"><h2>Servers</h2><canvas id="chartServers"></canvas></div>
+    <div class="col-md-6"><h2><span class="glyphicon glyphicon-time" aria-hidden="true"></span> Messages per hour</h2><canvas id="chartMsgsPerHour"></canvas></div>
+    <div class="col-md-6"><h2><span class="glyphicon glyphicon-cd" aria-hidden="true"></span> Servers</h2><canvas id="chartServers"></canvas></div>
   </div>
   <div class="row">
-    <div class="col-md-4"><h2>Message levels relative distribution</h2><canvas id="chartLevels"></canvas></div>
-    <div class="col-md-4"><h2>Messages by server</h2><canvas id="chartServersPie"></canvas></div>
+    <div class="col-md-4"><h2><span class="glyphicon glyphicon-signal" aria-hidden="true"></span> Message levels relative distribution</h2><canvas id="chartLevels"></canvas></div>
+    <div class="col-md-4"><h2><span class="glyphicon glyphicon-cd" aria-hidden="true"></span> Messages by server</h2><canvas id="chartServersPie"></canvas></div>
     <div class="col-md-4">
-      <h2>Database</h2>
+      <h2><span class="glyphicon glyphicon-wrench" aria-hidden="true"></span> Database</h2>
       <p>Events in DB: <?= $aStatistic->cnt ?><br>Oldest entry: <?= $aStatistic->oldest ?></p>
     </div>
   </div>
