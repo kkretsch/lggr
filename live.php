033c89826a96d375d844af8688aab0d9e3bba9c5
Kai Kretschmann <kai@kaikretschmann.de>
2015-07-17 10:00:10 +0200

    Live icon

--- live.php
+++ live.php
@@ -45,7 +45,7 @@ if(0 == count($aEvents)) {
 </div>
 
 <div class="container">
-	<h2>Live view <span id="tslatest"></span></h2>
+	<h2><span class="glyphicon glyphicon-refresh" aria-hidden="true"></span> Live view <span id="tslatest"></span></h2>
 </div>
 
 <div class="container datablock">
