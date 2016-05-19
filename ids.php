c1a03dd18142301550496a0cf6164c5796f9b7ed
Kai Kretschmann <kai@kaikretschmann.de>
2016-05-19 15:45:05 +0200

    mod_security redirect target page added

---
+++ ids.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Error - access denied</title>
+		<meta charset="utf-8">
+		<meta name="robots" content="noindex">
+		<style type="text/css">
+body {
+	margin: 0;
+	padding: 0;
+	font-family: Verdana, Arial, sans-serif;
+	background-color: #000;
+	color: #f00;
+}
+#wrapper {
+	width: 500px;
+	margin: 0 auto;
+}
+		</style>
+	</head>
+	<body>
+		<div id="wrapper">
+			<h1>Access denied!</h1>
+			<p>Your request has been blocked for security reasons. Please try again in a few minutes.
+Should the problem persist, please email me (see the contact page for details) with the
+following unique ID, which will help to identify your request:<br>
+			<tt><?php echo htmlspecialchars($_SERVER["REDIRECT_UNIQUE_ID"], ENT_QUOTES, 'UTF-8'); ?></tt></p>
+		</div><!-- wrapper -->
+	</body>
+</html>
