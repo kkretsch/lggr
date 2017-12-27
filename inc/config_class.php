457d5014b8a95b51c0bb6391ddc6faadf09e9b91
lggr <kai@kaikretschmann.de>
2017-12-27 10:39:15 +0100

    default to remote 3rd party tools, embedded contrib path now optional

--- inc/config_class.php
+++ inc/config_class.php
@@ -11,22 +11,21 @@ class Config extends AbstractConfig {
 		$this->setLocale('en_US');
 
 /* remote storage */
-/*
 		$this->setUrlBootstrap('//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/');
 		$this->setUrlJquery('//code.jquery.com/');
 		$this->setUrlJqueryui('//code.jquery.com/ui/1.11.4/');
 		$this->setUrlJAtimepicker('//cdnjs.cloudflare.com/ajax/libs/jquery-ui-timepicker-addon/1.4.5/');
 		$this->setUrlChartjs('//cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/');
-*/
 
 /* local storage */
+/*
 		$this->setUrlBootstrap('/contrib/bootstrap/');
 		$this->setUrlJquery('/contrib/jquery/');
 		$this->setUrlJqueryui('/contrib/jqueryui/');
 		$this->setUrlJAtimepicker('/contrib/timepicker/');
 		$this->setUrlChartjs('/contrib/chartjs/');
 		$this->setUrlJQCloud('/contrib/jqcloud/');
-
+*/
 	} // constructor
 
 } // class
